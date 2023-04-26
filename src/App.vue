fc6f9e26ff4c251a33e9797a88bd32cfdb042ffd
raffaele-catalano <raf.catalano@gmail.com>
2023-04-26 19:01:45 +0200

    implemented AppHero component + minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero  from './components/AppHero.vue'
 
 export default {
   name: 'App',
   components: {
     AppTitle,
-    // AppHero
+    AppHero
   },
   data() {
     return {
@@ -16,39 +17,24 @@ export default {
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <h1> {{ title }} </h1>
+
+  <AppHero />
+
+  <h1 id="main-title"> {{ title }} </h1>
+
   <AppTitle />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em rgba(100, 108, 255, 0.667));
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 
-h1 {
+#main-title {
   color: gold;
   will-change: filter;
   transition: filter 300ms;
 }
 
-h1:hover {
+#main-title:hover {
   filter: drop-shadow(0 0 1em rgb(255, 215, 0));
   cursor: pointer;
 }
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -3,14 +3,14 @@ export default {
     name: 'AppTitle',
     data() {
         return {
-            title: 'This is my First App with Vite',
+            title: 'This is my First App with',
         }
     }
 }
 </script>
 
 <template>
-    <h2>{{ title }}</h2>
+    <h2>{{ title }} <span>Vite</span></h2>
 </template>
 
 <style scoped>
@@ -24,4 +24,9 @@ export default {
         filter: drop-shadow(0 0 1em rgb(213, 19, 58));
         cursor: pointer;
     }
+
+    span {
+        color: rgb(147, 106, 254);
+    }
+
 </style>

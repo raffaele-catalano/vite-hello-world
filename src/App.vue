e3f6d63003e38c5ba54b48a58918384c8c8c8024
raffaele-catalano <raf.catalano@gmail.com>
2023-04-26 17:30:20 +0200

    implemented AppTitle components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTitle,
+    // AppHero
+  },
+  data() {
+    return {
+      title: 'Hello There!'
+    }
+  }
+}
 </script>
 
 <template>
@@ -11,7 +24,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1> {{ title }} </h1>
+  <AppTitle />
 </template>
 
 <style scoped>
@@ -22,9 +36,20 @@ import HelloWorld from './components/HelloWorld.vue'
   transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em rgba(100, 108, 255, 0.667));
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+h1 {
+  color: gold;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+h1:hover {
+  filter: drop-shadow(0 0 1em rgb(255, 215, 0));
+  cursor: pointer;
 }
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            title: 'This is my First App with Vite',
+        }
+    }
+}
+</script>
+
+<template>
+    <h2>{{ title }}</h2>
+</template>
+
+<style scoped>
+    h2 {
+        color: white;
+        will-change: filter;
+        transition: filter 300ms;
+    }
+
+    h2:hover {
+        filter: drop-shadow(0 0 1em rgb(213, 19, 58));
+        cursor: pointer;
+    }
+</style>
